af65d5d925219afac68e1eb2c4456b4cfdd4056d
Tarren Bos <tarren.bos@gmail.com>
2019-02-26 22:02:40 -0600

    Added DjangoProject
    Added the django project for the site.

---
+++ DjangoProject/channelx/users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+def register(request): 
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save() 
+            fname = form.cleaned_data.get('first_name')
+            lname = form.cleaned_data.get('last_name')
+            messages.success(request, f'Account created for {fname} {lname}!')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+

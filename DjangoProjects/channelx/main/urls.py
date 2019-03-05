736b9cbd247c8933a6d7f69cdf6a91bf80485c0b
Tarren Bos <tarren.bos@gmail.com>
2019-03-04 21:15:34 -0600

    Fixed the page smashing problem
    The pages no longer smash the entities together.

--- DjangoProjects/channelx/main/urls.py
+++ DjangoProjects/channelx/main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from users import views as users_views
 
 urlpatterns = [
     path('', views.home, name='main-home'),
+    path('register/', users_views.register, name='users-register'),
     path('about/', views.about, name='main-about'),
     path('createchannel/', views.createchannel, name='main-createchannel'),
     path('findchannel/', views.findchannel, name='main-findchannel'),

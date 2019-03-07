8536881d451936a1d06e26e35941a99ffa823532
Tarren Bos <tarren.bos@gmail.com>
2019-03-07 10:17:41 -0600

    Revert "Revert "Worked on getting createChannel form done""
    This reverts commit 67251be9bbbcc78cb8ff669705c08f417128aacf.

--- DjangoProjects/channelx/main/models.py
+++ DjangoProjects/channelx/main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Channel(models.Model):
+    channel_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.channel_name

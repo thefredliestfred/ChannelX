3802fbaed3fd0840eaae5e8df9afb360362c2ac7
Tarren Bos <tarren.bos@gmail.com>
2019-03-07 10:43:53 -0600

    Reverted Back before ABbie's push

--- DjangoProjects/channelx/main/models.py
+++ DjangoProjects/channelx/main/models.py
@@ -1,11 +1,3 @@
 from django.db import models
-#from django.contrib.auth.models import User
 
 # Create your models here.
-#class Chat(models.Model):
-#    created = models.DateTimeField(auto_now_add=True)
-#    user = models.ForeignKey('User', on_delete=models.DO_NOTHING,)
-#    message = models.TextField()
-
-#    def __unicode__(self):
-#        return self.message

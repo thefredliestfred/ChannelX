dc9176a83df3fe591aaaf4a5840b0d186ec764da
Tarren Bos <tarren.bos@gmail.com>
2019-03-21 10:52:04 -0500

    Deletes new message

--- DjangoProjects/channelx/main/urls.py
+++ DjangoProjects/channelx/main/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('channelinfo/', views.channelinfopage, name='main-channelinfo'),
     path('channelsettings/', views.channelsettingspage, name='main-channelsettings'),
     path('userprofile/', views.userprofilepage, name='main-userprofile'),
-    path('newmessage', views.newmessagepage, name='main-newmessage'),
+    #path('newmessage', views.newmessagepage, name='main-newmessage'),
 ]

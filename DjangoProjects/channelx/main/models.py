15838a7fe0a9a8d41756c50fe9a368a538fec3dd
Plexi-Glass <tarren.bos@gmail.com>
2019-03-24 18:20:18 -0500

    Fixed Create Channel

--- DjangoProjects/channelx/main/models.py
+++ DjangoProjects/channelx/main/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from datetime import date, datetime
 
 class Channel(models.Model):
     channel_name = models.CharField(max_length=30)
-    expire_date = models.DateField()
-    start_quiet_hour = models.TimeField()
-    end_quiet_hour = models.TimeField()
+    expire_date = models.DateField(default=date.today, null=True, blank=True)
+    start_quiet_hour = models.TimeField(default=datetime.now, blank=True)
+    end_quiet_hour = models.TimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return self.channel_name

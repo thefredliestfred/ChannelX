f5da1edefc97977a72dc7da27c311f2df2a56424
RollingDice88 <rollingdice12@gmail.com>
2019-04-28 19:06:07 -0500

    Can now fix profile picture

--- DjangoProjects/channelx/users/views.py
+++ DjangoProjects/channelx/users/views.py
@@ -27,7 +27,7 @@ def register(request):
 @login_required
 def profile(request):
     if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user,)
+        u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST,
                                    request.FILES,
                                    instance=request.user.profile)
@@ -36,12 +36,14 @@ def profile(request):
             p_form.save()
             messages.success(request, f'Your account has been updated!')
             return redirect('profile')
+
     else:
         u_form = UserUpdateForm(instance=request.user)
-#        p_form = ProfileUpdateForm(instance=request.user.profile)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form': u_form,
-#        'p_form': p_form
+        'p_form': p_form
     }
+
     return render(request, 'users/profile.html', context)

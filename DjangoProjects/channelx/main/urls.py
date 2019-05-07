05b9badf90f3979991f09eca9a04ea50566de3c2
Tarren Bos <tarren.bos@gmail.com>
2019-05-07 01:34:38 -0500

    Minor tweaks to UI

--- DjangoProjects/channelx/main/urls.py
+++ DjangoProjects/channelx/main/urls.py
@@ -5,6 +5,7 @@ from . import views
 from users import views as users_views
 from . import views
 from .views import (
+    MemberListView,
     ChannelListView,
     ChannelDetailView,
     ChannelCreateView,
@@ -23,7 +24,6 @@ urlpatterns = [
     path('channelsettings/', views.channelsettingspage, name='main-channelsettings'),
     path('thankyouregister/', views.thankyouregisterpage, name='main-thankyouregister'),
     path('ticketrecieved/', views.ticketrecivedpage, name='main-ticketrecieved'),
-    #path('ticketrequest/', views.ticketrequestpage, name='main-ticketrequest'),
     path('channel/', ChannelListView.as_view(), name='main-channel-list'),
     path('channel/new/', ChannelCreateView.as_view(), name='channel-create'),
     path('channel/<str:slug>/', ChannelDetailView.as_view(), name='channel-detail'),
